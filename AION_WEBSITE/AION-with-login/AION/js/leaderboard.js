1b23cd88ce19ddf1e6f942974c0177480a8e455f
pookie-bear11 <ethangabrieldelapena@gmail.com>
2026-09-01 22:18:02 +0800

    Delete AION_WEBSITE/AION-with-login/AION directory

--- AION_WEBSITE/AION-with-login/AION/js/leaderboard.js
+++
@@ -1,6 +0,0 @@
-import { leaderboardBase } from "./data.js";
-
-export function getLeaderboard(player) {
-  return [...leaderboardBase, { name: player.name, xp: player.totalXp, level: player.level, icon: "✦", isPlayer: true }]
-    .sort((a, b) => b.xp - a.xp);
-}
